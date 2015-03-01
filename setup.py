c9d1b415102e6c8f6981e76518eec4d8681c14b9
yetone <i@yetone.net>
2015-03-01 19:28:09 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
-version = '0.0.2'
+version = '0.0.3'
 
 setup(
     name='script-manager',
     version=version,
     keywords=('command-line interface', 'flask_script', 'argparse', 'script'),
     description='A command-line interface. A simple and crude implementation of Flask-Script.',
+    url='http://github.com/yetone/script-manager',
     license='MIT License',
     author='yetone',
     author_email='i@yetone.net',
